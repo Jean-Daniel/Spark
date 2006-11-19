fc611a8259dcaf95bfe7de4abc23ea438791c7e4
Jean-Daniel Dupas <jddupas@shadowlab.org>
2006-11-19 21:16:31 +0000

    Create Helper

---
+++ SparkEditor/Helper/SparkHelper.h
@@ -0,0 +1,16 @@
+/*
+ *  SparkHelper.h
+ *  Spark Editor
+ *
+ *  Created by Grayfox on 19/11/06.
+ *  Copyright 2006 Shadow Lab. All rights reserved.
+ */
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SparkHelper : NSObject {
+
+}
+
+@end
